bfaf4c7fb7d7e9f7d84fe33326764c22ba870f00
Damian Elenbaas <66534349+damian-elenbaas@users.noreply.github.com>
2023-04-12 22:45:27 +0200

    Enabled Color Column

--- lua/magisch/set.lua
+++ lua/magisch/set.lua
@@ -1,4 +1,4 @@
-vim.opt.guicursor = [[n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20]] 
+vim.opt.guicursor = [[n-v-c-sm:block,i-ci-ve:ver25,r-cr-o:hor20]]
 
 vim.opt.nu = true
 vim.opt.relativenumber = true
@@ -26,5 +26,5 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
--- vim.opt.colorcolumn = "80"
+vim.opt.colorcolumn = "80"
 vim.cmd('language en_US')
