ce88e07785ddbd78c71f84b24bdf174c7e319cd1
Damian Elenbaas <66534349+damian-elenbaas@users.noreply.github.com>
2023-06-08 18:27:44 +0200

    Added copilot

--- lua/magisch/packer.lua
+++ lua/magisch/packer.lua
@@ -76,5 +76,9 @@ return require('packer').startup(function(use)
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
     }
+
+    use {
+        'github/copilot.vim'
+    }
     -- use('andweeb/presence.nvim')
 end)
