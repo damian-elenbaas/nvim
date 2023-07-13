2ebd14f4697e97731cd7bc7a3d7dda85dbe5b0ec
Damian Elenbaas <damianelenbaas2003@gmail.com>
2023-07-13 23:18:18 +0200

    New plugs

--- lua/magisch/packer.lua
+++ lua/magisch/packer.lua
@@ -1,4 +1,4 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
+
 
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
@@ -12,10 +12,14 @@ return require('packer').startup(function(use)
         requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-    use { "catppuccin/nvim", as = "catppuccin" }
+    -- use { "catppuccin/nvim", as = "catppuccin" }
+    -- Tokio Night theme
+    use { 'folke/tokyonight.nvim', as = 'tokyonight' }
 
     use({'nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'}})
     use('nvim-treesitter/playground')
+    use('nvim-treesitter/nvim-treesitter-context')
+
     use('theprimeagen/harpoon')
     use('mbbill/undotree')
     use('tpope/vim-fugitive')
@@ -81,4 +85,5 @@ return require('packer').startup(function(use)
         'github/copilot.vim'
     }
     -- use('andweeb/presence.nvim')
+   -- use 'mfussenegger/nvim-jdtls'
 end)
