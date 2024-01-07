c59b0685e82de0ae96de845d65cc9359dbbd4466
Damian Elenbaas <damianelenbaas2003@gmail.com>
2024-01-07 21:19:37 +0100

    Changed settings

---
+++ lua/magisch/lazy/colorscheme.lua
@@ -0,0 +1,18 @@
+return {
+    -- {
+    --     "bluz71/vim-moonfly-colors",
+    --     name = "moonfly",
+    --     lazy = false,
+    --     config = function()
+    --         vim.cmd([[colorscheme moonfly]])
+    --     end
+    -- },
+    {
+        "morhetz/gruvbox",
+        name = "gruvbox",
+        lazy = false,
+        config = function()
+            vim.cmd([[colorscheme gruvbox]])
+        end
+    },
+}
--- lua/magisch/lazy/moonfly.lua
+++
@@ -1,7 +0,0 @@
-return {
-    "bluz71/vim-moonfly-colors", 
-    name = "moonfly",
-    config = function()
-        vim.cmd([[colorscheme moonfly]])
-    end
-}
