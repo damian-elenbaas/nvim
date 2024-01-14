fecb67e81fcd5cd4f5b228a9232505416ac8e983
Damian Elenbaas <damianelenbaas2003@gmail.com>
2024-01-14 15:11:45 +0100

    Added oil.nvim as file-explorer

--- lua/magisch/remap.lua
+++ lua/magisch/remap.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+-- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
