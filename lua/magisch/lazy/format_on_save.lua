580a56ee17a8d687b7f4880e67045a3f13ae0d78
Damian Elenbaas <damianelenbaas2003@gmail.com>
2024-01-24 22:34:38 +0100

    Added format on save

---
+++ lua/magisch/lazy/format_on_save.lua
@@ -0,0 +1,71 @@
+return {
+  "elentok/format-on-save.nvim",
+  config = function()
+    local format_on_save = require("format-on-save")
+    local formatters = require("format-on-save.formatters")
+
+    format_on_save.setup({
+      exclude_path_patterns = {
+        "/node_modules/",
+        ".local/share/nvim/lazy",
+      },
+      formatter_by_ft = {
+        css = formatters.lsp,
+        cs = formatters.lsp,
+        go = formatters.lsp,
+        html = formatters.lsp,
+        java = formatters.lsp,
+        javascript = formatters.lsp,
+        json = formatters.lsp,
+        lua = formatters.lsp,
+        markdown = formatters.prettierd,
+        openscad = formatters.lsp,
+        python = formatters.black,
+        rust = formatters.lsp,
+        scad = formatters.lsp,
+        scss = formatters.lsp,
+        sh = formatters.shfmt,
+        terraform = formatters.lsp,
+        typescript = formatters.prettierd,
+        typescriptreact = formatters.prettierd,
+        yaml = formatters.lsp,
+
+        -- Add your own shell formatters:
+        -- myfiletype = formatters.shell({ cmd = { "myformatter", "%" } }),
+
+        -- Add custom formatter
+        -- filetype1 = formatters.remove_trailing_whitespace,
+        -- filetype2 = formatters.custom({ format = function(lines)
+        --   return vim.tbl_map(function(line)
+        --     return line:gsub("true", "false")
+        --   end, lines)
+        -- end}),
+
+        -- Use a tempfile instead of stdin
+        -- go = {
+        --   formatters.shell({
+        --     cmd = { "goimports-reviser", "-rm-unused", "-set-alias", "-format", "%" },
+        --     tempfile = function()
+        --       return vim.fn.expand("%") .. '.formatter-temp'
+        --     end
+        --   }),
+        --   formatters.shell({ cmd = { "gofmt" } }),
+        -- },
+
+        -- Add conditional formatter that only runs if a certain file exists
+        -- in one of the parent directories.
+      },
+
+      -- Optional: fallback formatter to use when no formatters match the current filetype
+      fallback_formatter = {
+        formatters.remove_trailing_whitespace,
+        formatters.remove_trailing_newlines,
+        formatters.prettierd,
+      },
+
+      -- By default, all shell commands are prefixed with "sh -c" (see PR #3)
+      -- To prevent that set `run_with_sh` to `false`.
+      run_with_sh = false,
+    })
+  end
+}
