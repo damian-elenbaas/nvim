8c16d13b58d742855513009756beb7f34f685078
Damian Elenbaas <damianelenbaas2003@gmail.com>
2024-01-26 01:50:37 +0100

    Blocked no information available notification

--- lua/magisch/lazy/notify.lua
+++ lua/magisch/lazy/notify.lua
@@ -2,6 +2,14 @@ return {
   "rcarriga/nvim-notify",
   lazy = false,
   config = function()
-    vim.notify = require("notify")
+    local banned_messages = { "No information available" }
+    vim.notify = function(msg, ...)
+      for _, banned in ipairs(banned_messages) do
+        if msg == banned then
+          return
+        end
+      end
+      return require("notify")(msg, ...)
+    end
   end
 }
