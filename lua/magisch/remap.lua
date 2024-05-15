952d7b56b61f23ac3af6edbc986c464e838ca1a7
Damian Elenbaas <damianelenbaas2003@gmail.com>
2024-05-15 19:46:06 +0200

    Enabled Oil again

--- lua/magisch/remap.lua
+++ lua/magisch/remap.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
-vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
+-- vim.keymap.set("n", "<leader>e", vim.cmd.Ex)
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set("i", "<C-c>", "<Esc>")
 vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
