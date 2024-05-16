2dd6791fc13791a94f2c320b4fa79e2cd91fc41c
Damian Elenbaas <damianelenbaas2003@gmail.com>
2024-05-16 21:43:56 +0200

    Added symbols to completion list

--- lua/magisch/lazy/lsp.lua
+++ lua/magisch/lazy/lsp.lua
@@ -10,6 +10,8 @@ return {
     { 'hrsh7th/cmp-nvim-lsp' },
     { 'hrsh7th/cmp-nvim-lsp-signature-help' },
     { 'L3MON4D3/LuaSnip' },
+    { 'saadparwaiz1/cmp_luasnip' },
+    { 'onsails/lspkind.nvim' },
     -- Optional
     { 'williamboman/mason.nvim' },
     { 'williamboman/mason-lspconfig.nvim' },
@@ -29,6 +31,8 @@ return {
     local cmp = require('cmp')
     local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
+    local lspkind = require('lspkind')
+
     cmp.setup({
       window = {
         completion = cmp.config.window.bordered(),
@@ -45,7 +49,17 @@ return {
       sources = {
         { name = 'nvim_lsp' },
         { name = 'luasnip' },
-        { name = 'nvim_lsp_signature_help' }
+        { name = 'nvim_lsp_signature_help' },
+      },
+      formatting = {
+        format = lspkind.cmp_format({
+          mode = 'symbol', -- show only symbol annotations
+          maxwidth = 50,   -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+          -- can also be a function to dynamically calculate max width such as
+          -- maxwidth = function() return math.floor(0.45 * vim.o.columns) end,
+          ellipsis_char = '...',    -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
+          show_labelDetails = true, -- show labelDetails in menu. Disabled by default
+        })
       }
     })
 
