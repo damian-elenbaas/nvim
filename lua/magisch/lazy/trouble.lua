def4622f750e8382b4e62b25ca5fba847b44d608
Damian Elenbaas | ProAutNorm B.V. <damian.elenbaas@proautnorm.com>
2024-09-18 08:21:19 +0200

    Updated trouble config

--- lua/magisch/lazy/trouble.lua
+++ lua/magisch/lazy/trouble.lua
@@ -3,15 +3,38 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons"
   },
-  config = function()
-    local trouble = require("trouble")
-    trouble.setup()
-
-    vim.keymap.set("n", "<leader>xx", function() trouble.open() end)
-    vim.keymap.set("n", "<leader>xw", function() trouble.open("workspace_diagnostics") end)
-    vim.keymap.set("n", "<leader>xd", function() trouble.open("document_diagnostics") end)
-    vim.keymap.set("n", "<leader>xq", function() trouble.open("quickfix") end)
-    vim.keymap.set("n", "<leader>xl", function() trouble.open("loclist") end)
-    vim.keymap.set("n", "gR", function() trouble.open("lsp_references") end)
-  end
+  opts = {},
+  cmd = "Trouble",
+  keys = {
+    {
+      "<leader>xx",
+      "<cmd>Trouble diagnostics toggle<cr>",
+      desc = "Diagnostics (Trouble)",
+    },
+    {
+      "<leader>xX",
+      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+      desc = "Buffer Diagnostics (Trouble)",
+    },
+    {
+      "<leader>cs",
+      "<cmd>Trouble symbols toggle focus=false<cr>",
+      desc = "Symbols (Trouble)",
+    },
+    {
+      "<leader>cl",
+      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+      desc = "LSP Definitions / references / ... (Trouble)",
+    },
+    {
+      "<leader>xl",
+      "<cmd>Trouble loclist toggle<cr>",
+      desc = "Location List (Trouble)",
+    },
+    {
+      "<leader>xq",
+      "<cmd>Trouble qflist toggle<cr>",
+      desc = "Quickfix List (Trouble)",
+    },
+  }, 
 }
