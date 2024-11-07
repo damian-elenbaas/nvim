dce6eb5d23bcfb22086e07fe34134394b6d58225
Damian Elenbaas | ProAutNorm B.V. <damian.elenbaas@proautnorm.com>
2024-11-07 07:37:10 +0100

    New plugins

---
+++ lua/magisch/lazy/conform.lua
@@ -0,0 +1,21 @@
+return {
+  'stevearc/conform.nvim',
+  opts = {},
+  config = function()
+    require("conform").setup({
+      formatters_by_ft = {
+        -- You can customize some of the format options for the filetype (:help conform.format)
+        rust = { "rustfmt", lsp_format = "fallback" },
+        -- Conform will run the first available formatter
+        javascript = { "prettierd", "prettier", stop_after_first = true },
+        typescript = { "prettierd", "prettier", stop_after_first = true },
+        cs = { "csharpier", "omnisharp", stop_after_first = true },
+      },
+      format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_format = "fallback",
+      },
+    })
+  end
+}
