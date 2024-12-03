4807fb7c5c8581d0db028034a84204edb92f9dd4
Damian Elenbaas | ProAutNorm B.V. <damian.elenbaas@proautnorm.com>
2024-12-03 08:13:54 +0100

    copilot keybind change

--- lua/magisch/lazy/copilot.lua
+++ lua/magisch/lazy/copilot.lua
@@ -32,7 +32,7 @@ return {
           hide_during_completion = true,
           debounce = 75,
           keymap = {
-            accept = "<M-l>",
+            accept = "<M-\\>",
             accept_word = false,
             accept_line = false,
             next = "<M-]>",
