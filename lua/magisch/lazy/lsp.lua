db9420b48e2bb7daaeb9d2825429ab006794dc0d
Damian Elenbaas | ProAutNorm B.V. <damian.elenbaas@proautnorm.com>
2025-01-24 11:22:24 +0100

    Vim lua lib added to LuaLSP

--- lua/magisch/lazy/lsp.lua
+++ lua/magisch/lazy/lsp.lua
@@ -165,6 +165,12 @@ return {
               Lua = {
                 diagnostics = {
                   globals = { 'vim' }
+                },
+                workspace = {
+                  library = {
+                    vim.fn.expand('$VIMRUNTIME/lua'),
+                    vim.fn.expand('$VIMRUNTIME/lua/vim/lsp'),
+                  }
                 }
               }
             }
