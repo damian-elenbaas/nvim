b73719872225c042dd1c73aa4300151ad1178cc6
Damian Elenbaas <damianelenbaas2003@gmail.com>
2025-03-25 15:22:06 +0100

    y

--- lua/magisch/lazy/conform.lua
+++ lua/magisch/lazy/conform.lua
@@ -1,11 +1,12 @@
 return {
   'stevearc/conform.nvim',
+  enabled = true,
   opts = {},
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "prettierd", stop_after_first = true },
+        typescript = { "prettierd", stop_after_first = true },
         php = {},
         -- cs = { "csharpier" },
       },
