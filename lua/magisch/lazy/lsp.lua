150d4b644f8af64c06d316b24df23adb4789ab65
Damian Elenbaas <damianelenbaas2003@gmail.com>
2025-06-27 10:31:17 +0200

    Added Octo & Svelte lsp

--- lua/magisch/lazy/lsp.lua
+++ lua/magisch/lazy/lsp.lua
@@ -136,6 +136,11 @@ return {
         filetypes = { "json", "jsonc" },
       }
 
+      -- Svelte
+      lspconfig.svelte.setup {
+        capabilities = capabilities,
+      }
+
       vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           local bufnr = args.buf
