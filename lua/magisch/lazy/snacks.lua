b00896d8457fcc5a3346ab67630e7cb87308ba3a
Damian Elenbaas <damianelenbaas2003@gmail.com>
2026-04-24 11:11:25 +0200

    Enabled snacks.image

--- lua/magisch/lazy/snacks.lua
+++ lua/magisch/lazy/snacks.lua
@@ -7,7 +7,7 @@ return {
       explorer = { enabled = false },
       indent = { enabled = false },
       input = { enabled = false },
-      image = { enabled = false },
+      image = { enabled = true },
       picker = { enabled = true },
       notifier = { enabled = false },
       quickfile = { enabled = true },
