6ba86d445f4af8fdd535c8298747c58c151caa37
Damian Elenbaas <damian@stijlbreuk.nl>
2026-09-18 14:46:25 +0200

    feat: Added oxfmt as the primary JS/TS formatter
    Only runs where the project has an .oxfmtrc.json(c), otherwise it falls
    through to prettierd. Bumps format_on_save to 2000ms because prettierd
    needs longer on its first run while the daemon boots.

--- lua/magisch/lazy/conform.lua
+++ lua/magisch/lazy/conform.lua
@@ -3,15 +3,27 @@ return {
   config = function()
     require("conform").setup({
       formatters_by_ft = {
-        javascript = { "prettierd", "prettier", stop_after_first = true },
-        typescript = { "prettierd", "prettier", stop_after_first = true },
-        typescriptreact = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { "oxfmt", "prettierd", stop_after_first = true },
+        typescript = { "oxfmt", "prettierd", stop_after_first = true },
+        typescriptreact = { "oxfmt", "prettierd", stop_after_first = true },
         php = {},
         -- cs = { "csharpier" },
       },
+      formatters = {
+        -- Only use oxfmt in projects that have an oxfmt config; otherwise fall through to prettier
+        oxfmt = {
+          condition = function(_, ctx)
+            return vim.fs.find({ ".oxfmtrc.json", ".oxfmtrc.jsonc" }, {
+              path = ctx.filename,
+              upward = true,
+            })[1] ~= nil
+          end,
+        },
+      },
       format_on_save = {
         -- These options will be passed to conform.format()
-        timeout_ms = 500,
+        -- prettierd needs longer on its first run (daemon startup + loading prettier/plugins)
+        timeout_ms = 2000,
         lsp_format = "fallback",
       },
     })
